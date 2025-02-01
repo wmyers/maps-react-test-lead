956811b2b50261e6ff2097c6905127c53c01e990
Will Myers <will@therecruitmentnetwork.com>
2025-02-01 00:18:22 +0000

    wire up results component to calculations

--- app/ui/Results.tsx
+++ app/ui/Results.tsx
@@ -1,11 +1,29 @@
 import { Table } from 'react-bootstrap';
 import { formatCurrency } from '../lib/utils/formatCurrency';
 import { ComponentProps } from '../lib/definitions';
+import {
+  calculateAffordabilityCheckMonthlyPayment,
+  calculateCapital,
+  calculateMonthlyPayment,
+  calculateTotalRepayment,
+  calculateWholeTermInterest,
+} from '../lib/utils/MortgageCalculator/calculateRepayment';
+import coerceStringsToNumbers from '../lib/utils/coerceStringsToNumbers';
 
 export default function Results({ inputValues }: ComponentProps) {
-  const { price, deposit, term, interest } = inputValues;
+  const { price, deposit, term, interest } = coerceStringsToNumbers(inputValues);
 
-  // use search param values to calculate mortgage fields
+  const monthlyPayment = calculateMonthlyPayment(price, deposit, interest, term);
+  const totalRepayment = calculateTotalRepayment(monthlyPayment, term);
+  const capital = calculateCapital(price, deposit);
+  const wholeTermInterest = calculateWholeTermInterest(totalRepayment, capital);
+  const affordabilityCheckMonthlyPayment = calculateAffordabilityCheckMonthlyPayment(
+    price,
+    deposit,
+    interest,
+    term,
+  );
+
   return (
     <>
       <h2 className="pb-3">Results</h2>
@@ -13,23 +31,25 @@ export default function Results({ inputValues }: ComponentProps) {
         <tbody>
           <tr className="border-b border-t">
             <td>Monthly Payment</td>
-            <td className="text-right">{formatCurrency(763.68)}</td>
+            <td className="text-right">{formatCurrency(monthlyPayment)}</td>
           </tr>
           <tr className="border-b">
             <td>Total Repayment</td>
-            <td className="text-right">{formatCurrency(137463.09)}</td>
+            <td className="text-right">{formatCurrency(totalRepayment)}</td>
           </tr>
           <tr className="border-b">
             <td>Capital</td>
-            <td className="text-right">{formatCurrency(95000)}</td>
+            <td className="text-right">{formatCurrency(capital)}</td>
           </tr>
           <tr className="border-b">
             <td>Interest</td>
-            <td className="text-right">{formatCurrency(42463.09)}</td>
+            <td className="text-right">{formatCurrency(wholeTermInterest)}</td>
           </tr>
           <tr className="border-b">
             <td>Affordability check</td>
-            <td className="text-right">{formatCurrency(921.63)}</td>
+            <td className="text-right">
+              {formatCurrency(affordabilityCheckMonthlyPayment)}
+            </td>
           </tr>
         </tbody>
       </Table>
