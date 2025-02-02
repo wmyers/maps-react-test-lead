ac783aa65c62d14e2ce6379ebb20b0c5c6f3581e
Will Myers <will@therecruitmentnetwork.com>
2025-02-02 15:05:41 +0000

    refactor input default code using boe request into separate testable function

--- app/page.tsx
+++ app/page.tsx
@@ -9,24 +9,11 @@ import { FormInput } from './lib/definitions';
 import { getBankRate } from './lib/data/boe';
 import { validateAndParseInput } from './lib/validation/inputValues';
 
-export default async function MortgageCalculator(props: {
-  searchParams?: Promise<FormInput>;
+export default async function MortgageCalculatorUI(props: {
+  searchParams: Promise<FormInput>;
 }) {
   const searchParams = await props.searchParams;
-  const boeRate = await getBankRate();
-
-  // string inputs from search params
-  const input: FormInput = {
-    // default price to £100,000 if nullish search param
-    price: searchParams?.price ?? '100000',
-    // default deposit to £5,000 if nullish search param
-    // NB this param is still optionally defaulting to 0 when parsed by zod
-    deposit: searchParams?.deposit ?? '5000',
-    // default term to 15 years if nullish search param
-    term: searchParams?.term ?? '15',
-    // always use boe rate as default
-    interest: searchParams?.interest || boeRate.toString(),
-  };
+  const input = await getInput(searchParams);
 
   return (
     <Container>
@@ -65,4 +52,21 @@ export function Calculations({ input }: { input: FormInput }) {
       </Col>
     </>
   );
+}
+
+export async function getInput(searchParams: FormInput): Promise<FormInput> {
+  const boeRate = await getBankRate();
+
+  // string inputs from search params
+  return {
+    // default price to £100,000 if nullish search param
+    price: searchParams?.price ?? '100000',
+    // default deposit to £5,000 if nullish search param
+    // NB this param is still optionally defaulting to 0 when parsed by zod
+    deposit: searchParams?.deposit ?? '5000',
+    // default term to 15 years if nullish search param
+    term: searchParams?.term ?? '15',
+    // always use boe rate as default
+    interest: searchParams?.interest || boeRate.toString(),
+  };
 }
