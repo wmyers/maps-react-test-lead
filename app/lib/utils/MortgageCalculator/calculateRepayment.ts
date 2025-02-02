87e739c430c98ace5d74525f7138ab2517b9435c
Will Myers <will@therecruitmentnetwork.com>
2025-02-02 11:10:30 +0000

    add yearly balances function first pass

--- app/lib/utils/MortgageCalculator/calculateRepayment.ts
+++ app/lib/utils/MortgageCalculator/calculateRepayment.ts
@@ -1,38 +1,4 @@
 /**
- * Calculates the monthly mortgage payment.
- * Formula: M = P * (r * (1 + r)^n) / ((1 + r)^n - 1)
- *
- * @param propertyPrice - The price of the property.
- * @param deposit - The deposit amount.
- * @param annualInterestRate - The annual interest rate.
- * @param mortgageTermInYears - The mortgage term in years.
- * @returns The monthly mortgage payment.
- */
-
-export function calculateMonthlyPayment(
-  propertyPrice: number,
-  deposit: number,
-  annualInterestRate: number,
-  mortgageTermInYears: number,
-): number {
-  const adjustedLoanAmount = propertyPrice - deposit;
-  const monthlyInterestRate = annualInterestRate / 100 / 12;
-  const numberOfPayments = mortgageTermInYears * 12;
-
-  if (monthlyInterestRate === 0) {
-    return adjustedLoanAmount / numberOfPayments;
-  }
-
-  const monthlyPayment =
-    (adjustedLoanAmount *
-      monthlyInterestRate *
-      Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
-    (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
-
-  return monthlyPayment;
-}
-
-/**
  * Calculates the total repayment.
  * Formula: T = M * n
  *
@@ -50,6 +16,7 @@ export function calculateTotalRepayment(
 
 /**
  * Calculates the capital. This is the original sum borrowed. This excludes any interest and deposit.
+ * NB Capital is the same as Principal
  * Formula: C = P - D
  *
  * @param propertyPrice - The price of the property.
@@ -99,4 +66,93 @@ export function calculateAffordabilityCheckMonthlyPayment(
     annualInterestRate + 3,
     mortgageTermInYears,
   );
+}
+
+/**
+ * Calculates the monthly mortgage payment.
+ * Formula: M = P * (r * (1 + r)^n) / ((1 + r)^n - 1)
+ *
+ * @param propertyPrice - The price of the property.
+ * @param deposit - The deposit amount.
+ * @param annualInterestRate - The annual interest rate.
+ * @param mortgageTermInYears - The mortgage term in years.
+ * @returns The monthly mortgage payment.
+ */
+
+export function calculateMonthlyPayment(
+  propertyPrice: number,
+  deposit: number,
+  annualInterestRate: number,
+  mortgageTermInYears: number,
+): number {
+  const capital = calculateCapital(propertyPrice, deposit);
+  const monthlyInterestRate = annualInterestRate / 100 / 12;
+  const numberOfPayments = mortgageTermInYears * 12;
+
+  if (monthlyInterestRate === 0) {
+    return capital / numberOfPayments;
+  }
+
+  const monthlyPayment =
+    (capital *
+      monthlyInterestRate *
+      Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
+    (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+
+  return monthlyPayment;
+}
+
+/**
+ * Calculates the remaining mortgage balance at the end of each year.
+ * Formula: B = P * (1 + r)^n - (M * ((1 + r)^n - 1) / r)
+ * where:
+ * B = Balance
+ * P = Principal (loan amount) (aka Capital)
+ * r = Monthly interest rate
+ * n = Number of payments made
+ * M = Monthly payment
+ *
+ * @param propertyPrice - The price of the property
+ * @param deposit - The deposit amount
+ * @param annualInterestRate - The annual interest rate
+ * @param mortgageTermInYears - The mortgage term in years
+ * @returns Array of yearly balances, where index represents the year number
+ */
+export function calculateYearlyBalances(
+  propertyPrice: number,
+  deposit: number,
+  annualInterestRate: number,
+  mortgageTermInYears: number,
+): number[] {
+  const capital = calculateCapital(propertyPrice, deposit);
+  const monthlyInterestRate = annualInterestRate / 100 / 12;
+  const monthlyPayment = calculateMonthlyPayment(
+    propertyPrice,
+    deposit,
+    annualInterestRate,
+    mortgageTermInYears,
+  );
+
+  // Handle 0% interest rate separately
+  // In this case your monthly payment remains consistent throughout the term
+  if (monthlyInterestRate === 0) {
+    return Array.from({ length: mortgageTermInYears + 1 }, (_, year) => {
+      const remainingPayments = (mortgageTermInYears - year) * 12;
+      return remainingPayments * monthlyPayment;
+    });
+  }
+
+  // Calculate balance for each year
+  // This calculation uses the amortization formula for each year of the mortgage term
+  // https://en.wikipedia.org/wiki/Amortization_calculator
+  return Array.from({ length: mortgageTermInYears + 1 }, (_, year) => {
+    const paymentsCompleted = year * 12;
+    const balance =
+      capital * Math.pow(1 + monthlyInterestRate, paymentsCompleted) -
+      (monthlyPayment * (Math.pow(1 + monthlyInterestRate, paymentsCompleted) - 1)) /
+        monthlyInterestRate;
+
+    // Round to 0 decimal places
+    return Math.round(balance * 100) / 100;
+  });
 }
